b5066ccc2a6ff526395da4374e72a3f0efa52d2a
Makoto Tanabe <xekid78@outlook.com>
2018-03-22 16:41:19 +0900

    Add files via upload

---
+++ Inheri.java
@@ -0,0 +1,36 @@
+/**
+ *
+ * @author xekid78
+ *
+ */
+public class Inheri {
+
+	public static void main(String[] args) {
+		Box box = new Box();
+		box.open();
+
+		JewelryBox jewelrybox = new JewelryBox();
+		jewelrybox.look();
+		jewelrybox.open();
+
+	}
+
+}
+
+class Box {
+	public String myItem;
+
+	public Box() {
+		myItem = "新しいアイテム";
+	}
+
+	public void open() {
+		System.out.println("宝箱を開いた。" + myItem + "を手に入れた。");
+	}
+}
+
+class JewelryBox extends Box {
+	public void look() {
+		System.out.println("宝箱はキラキラと輝いている");
+	}
+}
